e7bee0874e848d38f99c8afe293ff7335e19d0a9
adesha <adeshaholagodu@gmail.com>
2024-08-18 21:32:01 +0530

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes:Routes=[
+  { path:'home',component:HomeComponent, title: "Home - Robot Shop"},
+  { path:'catalog',component:CatalogComponent, title: "Catalog - Robot Shop"},
+  { path:'cart',component:CartComponent, title: "Cart - Robot Shop"}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
